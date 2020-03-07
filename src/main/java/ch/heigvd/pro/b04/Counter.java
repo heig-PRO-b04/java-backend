279305f0a61f309c83839594d8a359d086859bc7
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-08 00:36:45 +0100

    Change the sample API to a counter
    Counters have mutable state that goes in both directions, and they're
    great for examples.

---
+++ src/main/java/ch/heigvd/pro/b04/Counter.java
@@ -0,0 +1,14 @@
+package ch.heigvd.pro.b04;
+
+public class Counter {
+
+  private final long value;
+
+  public Counter(long value) {
+    this.value = value;
+  }
+
+  public long getValue() {
+    return value;
+  }
+}
--- src/main/java/ch/heigvd/pro/b04/GreetingController.java
+++ src/main/java/ch/heigvd/pro/b04/GreetingController.java
@@ -1,7 +1,6 @@
 package ch.heigvd.pro.b04;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -9,11 +8,15 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @GetMapping("/greeting")
-  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  @GetMapping("/increment")
+  public Counter greeting() {
+    return new Counter(counter.incrementAndGet());
+  }
+
+  @GetMapping("/decrement")
+  public Counter decrement() {
+    return new Counter(counter.decrementAndGet());
   }
 }
