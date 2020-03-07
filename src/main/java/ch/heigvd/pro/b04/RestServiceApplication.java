a90e5caeb9466d4b1e7d7b896cf482bca4a79910
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-08 00:11:17 +0100

    Set the project up with Spring Boot MVC
    We're not sure if we want to use Spring Boot MVC, but it for sure
    provides some nice APIs to define API endpoints for REST services. Maybe
    we can even tie it to the login infrastructure.

---
+++ src/main/java/ch/heigvd/pro/b04/RestServiceApplication.java
@@ -0,0 +1,12 @@
+package ch.heigvd.pro.b04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestServiceApplication.class, args);
+  }
+}
