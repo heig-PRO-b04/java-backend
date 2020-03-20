f23b9f4f78eb4896774a8ad4687dd3113d686b4e
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-20 23:30:27 +0100

    Add missing JavaDoc to pass Checkstyle

--- src/main/java/ch/heigvd/pro/b04/RestServiceApplication.java
+++ src/main/java/ch/heigvd/pro/b04/RestServiceApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestServiceApplication {
 
+  /**
+   * The main entry point of the application.
+   * @param args Command-line arguments, passed when the app is run.
+   */
   public static void main(String[] args) {
     System.err.println("Running on " + System.getenv("SPRING_DATASOURCE_URL"));
     System.err.println("Username : " + System.getenv("SPRING_DATASOURCE_USERNAME"));
