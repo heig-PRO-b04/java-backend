23722cc9621f6fd1c78e1757c36c1ae2f1267fbf
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-28 21:52:27 +0100

    Replace @Controller with @RestController

--- src/main/java/ch/heigvd/pro/b04/endpoints/PollController.java
+++ src/main/java/ch/heigvd/pro/b04/endpoints/PollController.java
@@ -6,13 +6,13 @@ import ch.heigvd.pro.b04.polls.PollIdentifier;
 import ch.heigvd.pro.b04.polls.PollRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class PollController {
 
   @Autowired
