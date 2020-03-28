1dfee2b10db85f8bcec17becbecce87ead189930
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-28 21:58:14 +0100

    Remove unneeded repository implementation

--- src/main/java/ch/heigvd/pro/b04/polls/PollRepository.java
+++ src/main/java/ch/heigvd/pro/b04/polls/PollRepository.java
@@ -1,9 +1,7 @@
 package ch.heigvd.pro.b04.polls;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PollRepository extends JpaRepository<Poll, PollIdentifier>,
-    JpaSpecificationExecutor<Poll> {
+public interface PollRepository extends JpaRepository<Poll, PollIdentifier> {
 
 }
