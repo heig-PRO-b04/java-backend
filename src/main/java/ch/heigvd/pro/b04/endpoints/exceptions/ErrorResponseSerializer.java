ffcfe63c7a6da1ed831f11bd59d05d231849fdd2
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-29 12:58:09 +0200

    Add an ErrorResponse entity, with a serializer

---
+++ src/main/java/ch/heigvd/pro/b04/endpoints/exceptions/ErrorResponseSerializer.java
@@ -0,0 +1,22 @@
+package ch.heigvd.pro.b04.endpoints.exceptions;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import org.springframework.boot.jackson.JsonComponent;
+
+@JsonComponent
+public class ErrorResponseSerializer extends JsonSerializer<ErrorResponse> {
+
+  @Override
+  public void serialize(
+      ErrorResponse errorResponse,
+      JsonGenerator jsonGenerator,
+      SerializerProvider serializerProvider
+  ) throws IOException {
+    jsonGenerator.writeStartObject();
+    jsonGenerator.writeStringField("message", errorResponse.getMessage());
+    jsonGenerator.writeEndObject();
+  }
+}
