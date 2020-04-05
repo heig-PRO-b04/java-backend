683c2bbe33aa3490734c4091ca2c997799143425
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-05 20:05:34 +0200

    Add some documentation for Utils.hash

--- src/main/java/ch/heigvd/pro/b04/auth/Utils.java
+++ src/main/java/ch/heigvd/pro/b04/auth/Utils.java
@@ -6,6 +6,12 @@ import java.nio.charset.StandardCharsets;
 
 public class Utils {
 
+  /**
+   * Hashes a password string with a SHA512 function. Currently, a constant salt is used.
+   *
+   * @param password The String password to hash.
+   * @return A hashed, UTF-8 encoded version of the input.
+   */
   public static String hash(String password) {
     return Hashing.sha512()
         .hashString(password + Constants.HASH, StandardCharsets.UTF_8)
