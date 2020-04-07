4eb580c8c2f4348a58db2e6f1246e4d9142fcba1
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-07 22:58:05 +0200

    Add a test case for empty usernames

--- src/test/java/ch/heigvd/pro/b04/auth/RegistrationControllerTest.java
+++ src/test/java/ch/heigvd/pro/b04/auth/RegistrationControllerTest.java
@@ -1,6 +1,7 @@
 package ch.heigvd.pro.b04.auth;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import ch.heigvd.pro.b04.moderators.ModeratorRepository;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,17 @@ public class RegistrationControllerTest {
         .build();
 
     assertDoesNotThrow(() -> registrationController.register(credentials));
+  }
+
+  @Test
+  public void testRegistrationDoesNotWorkWithEmptyPassword() {
+
+    UserCredentials credentials = UserCredentials.builder()
+        .username("")
+        .password("password")
+        .build();
+
+    assertThrows(Throwable.class, () -> registrationController.register(credentials));
   }
 
 }
