327c61537490cd3310d76618341d41834c187b44
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-15 18:24:03 +0200

    Fix issue with poll updates

--- src/main/java/ch/heigvd/pro/b04/polls/ServerPollRepository.java
+++ src/main/java/ch/heigvd/pro/b04/polls/ServerPollRepository.java
@@ -2,7 +2,9 @@ package ch.heigvd.pro.b04.polls;
 
 import ch.heigvd.pro.b04.moderators.Moderator;
 import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ServerPollRepository extends JpaRepository<ServerPoll, ServerPollIdentifier> {
@@ -10,6 +12,8 @@ public interface ServerPollRepository extends JpaRepository<ServerPoll, ServerPo
   @Query("SELECT p FROM ServerPoll p WHERE p.idPoll.idxModerator = :moderator")
   List<ServerPoll> findAllByModerator(Moderator moderator);
 
+  @Transactional
+  @Modifying
   @Query("UPDATE ServerPoll p SET p.title = :title WHERE p.idPoll = :id")
   void update(ServerPollIdentifier id, String title);
 }
