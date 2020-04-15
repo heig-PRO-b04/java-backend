cc877fcee5c8db9cddcbd1cd6ce73fcbac03daf4
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-15 13:43:44 +0200

    Move question visibility to its own file

--- src/main/java/ch/heigvd/pro/b04/Constants.java
+++ src/main/java/ch/heigvd/pro/b04/Constants.java
@@ -11,10 +11,6 @@ public class Constants {
 
   public static final String HASH = "Tu-144";
 
-  public enum QuestionVisbility {
-    VISIBLE, HIDDEN, ARCHIVED
-  }
-
   public enum SessionState {
     OPEN, CLOSED_TO_NEW_ONES, CLOSED
   }
