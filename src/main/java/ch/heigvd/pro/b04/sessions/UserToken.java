7a0278da1b1eab7a5c199d77a001c3666f5de832
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-04-16 10:28:06 +0200

    Implement UserToken and make it json serializable

--- src/main/java/ch/heigvd/pro/b04/sessions/UserToken.java
+++ src/main/java/ch/heigvd/pro/b04/sessions/UserToken.java
@@ -1,4 +1,34 @@
 package ch.heigvd.pro.b04.sessions;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.boot.jackson.JsonComponent;
+
+/**
+ * A class representing a Token, concerning users
+ */
+@Builder
+@Data
 public class UserToken {
+  private String token;
+
+  @JsonComponent
+  public static class UserTokenJsonSerializer extends
+      JsonSerializer<UserToken> {
+
+    @Override
+    public void serialize(
+          UserToken userToken
+        , JsonGenerator jsonGenerator
+        , SerializerProvider serializerProvider
+    ) throws IOException {
+      jsonGenerator.writeStartObject();
+      jsonGenerator.writeStringField("token", userToken.token);
+      jsonGenerator.writeEndObject();
+    }
+  }
 }
