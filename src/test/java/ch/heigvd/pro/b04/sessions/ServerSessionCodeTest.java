31254dab72518f3ace988792c1b6fa0fe9447d7f
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-04-23 07:12:46 +0200

    Add RepeatedTest to make sure our SessionCode are the right length

--- src/test/java/ch/heigvd/pro/b04/sessions/ServerSessionCodeTest.java
+++ src/test/java/ch/heigvd/pro/b04/sessions/ServerSessionCodeTest.java
@@ -3,6 +3,7 @@ package ch.heigvd.pro.b04.sessions;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class ServerSessionCodeTest {
@@ -29,5 +30,11 @@ public class ServerSessionCodeTest {
   public void testFormatHexadecimal() {
     SessionCode code = SessionCode.builder().hexadecimal("0xABCD").build();
     assertTrue(SessionCode.conformsToFormat(code));
+  }
+
+  @RepeatedTest(150)
+  public void testGeneratedSessionCodeIsCorrectLength() {
+    String code = ServerSession.createSessionCode();
+    assertTrue(code.length() == 6);
   }
 }
