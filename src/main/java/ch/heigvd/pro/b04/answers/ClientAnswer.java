974bed38e647cd8235f9a7b4633c373e187514de
David <duprazdavid@gmail.com>
2020-04-28 16:21:23 +0200

    Add a deserializer for client answers

--- src/main/java/ch/heigvd/pro/b04/answers/ClientAnswer.java
+++ src/main/java/ch/heigvd/pro/b04/answers/ClientAnswer.java
@@ -1,11 +1,42 @@
 package ch.heigvd.pro.b04.answers;
 
+import ch.heigvd.pro.b04.questions.ClientQuestion;
+import ch.heigvd.pro.b04.questions.QuestionVisibility;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.IOException;
+import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
+import org.springframework.boot.jackson.JsonComponent;
 
+@Builder
 public class ClientAnswer {
   @Getter
   private String text;
   @Getter
   private String description;
+
+  @JsonComponent
+  public static class Deserializer extends JsonDeserializer<ClientAnswer> {
+
+    @Override
+    public ClientAnswer deserialize(
+        JsonParser jsonParser,
+        DeserializationContext deserializationContext
+    ) throws IOException {
+      try {
+        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        return ClientAnswer.builder()
+            .text(node.get("title").asText())
+            .description(node.get("description").asText())
+            .build();
+      } catch (Exception anything) {
+        anything.printStackTrace();
+        throw anything;
+      }
+    }
+  }
 }
