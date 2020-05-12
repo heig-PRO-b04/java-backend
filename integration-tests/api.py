980d0aad4be6285b8e39aafce8855b5319f7a314
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-12 14:50:44 +0200

    Change function order

--- integration-tests/api.py
+++ integration-tests/api.py
@@ -4,6 +4,7 @@ import json
 
 server="http://localhost:80"
 
+# Moderator section
 def register(username, password):
     data = { "username" : username, "password" : password }
     url = server + "/register"
@@ -22,22 +23,24 @@ def auth(username, password):
     else:
         return { "error" : res.status_code }
 
+# User section
+
+def connectUser(code):
+    data = { "code" : code }
+    url = server + "/connect"
+    res = requests.post(url, json = data)
+    if (res.status_code == 200):
+        return res.json()["token"]
+    else:
+        return { "error" : res.status_code }
+
+# Poll section
 def createPoll(token, idMod, title):
     data = { "title" : title }
     url = server + "/mod/"+ str(idMod) + "/poll?token=" + token
     res = requests.post(url, json = data)
     if (res.status_code == 200):
         return res.json()["idPoll"]
-    else:
-        return { "error" : res.status_code }
-
-def addQuestion(token, idPoll, title):
-    data = { "title" : title, "details" : "This is a comment", "visibility" : "visible",
-            "answersMin" : 1, "answersMax" : 10 }
-    url = server + "/mod/"+ str(idMod) + "/poll/" + str(idPoll) + "/question?token=" + token
-    res = requests.post(url, json = data)
-    if (res.status_code == 200):
-        return
     else:
         return { "error" : res.status_code }
 
@@ -50,20 +53,22 @@ def sessionStatus(token, idPoll, status):
     else:
         return { "error" : res.status_code }
 
-def connectUser(code):
-    data = { "code" : code }
-    url = server + "/connect"
-    res = requests.post(url, json = data)
-    if (res.status_code == 200):
-        return res.json()["token"]
-    else:
-        return { "error" : res.status_code }
-
 def getSession(token):
     url = server + "/session?token=" + token
     res = requests.get(url)
     if (res.status_code == 200):
         return res.json()
+    else:
+        return { "error" : res.status_code }
+
+# Question section
+def addQuestion(token, idPoll, title):
+    data = { "title" : title, "details" : "This is a comment", "visibility" : "visible",
+            "answersMin" : 1, "answersMax" : 10 }
+    url = server + "/mod/"+ str(idMod) + "/poll/" + str(idPoll) + "/question?token=" + token
+    res = requests.post(url, json = data)
+    if (res.status_code == 200):
+        return
     else:
         return { "error" : res.status_code }
 
