ffcd281703f9afe6e93ea2e14a7cce3e5529f483
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-12 17:38:07 +0200

    Fix script

---
+++ integration-tests/launchIntegrationTests.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import subprocess
+
+path = r"."
+for f in os.listdir(path):
+    if (re.match('^test(.)+\.py$', f)):
+        command = "python3 " + path + "/" + f
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        print("Running " + command)
+        status = p.wait()
+
+        if (status != 0):
+            print ("Error in file: " + f)
+            sys.exit(1)
+
