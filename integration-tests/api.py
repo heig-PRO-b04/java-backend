ea52dcbfff4bebcd5b7e19905d084d350d7d0c69
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-12 14:47:45 +0200

    Remove global variables

--- integration-tests/api.py
+++ integration-tests/api.py
@@ -2,43 +2,36 @@
 import requests
 import json
 
-server = "http://172.22.22.53:8080"
-token = ""
-idMod = ""
+server="http://localhost:80"
 
-def register():
-    global token
+def register(username, password):
     data = { "username" : username, "password" : password }
     url = server + "/register"
     res = requests.post(url, json = data)
     if (res.status_code == 200):
-        token = res.json()["token"]
+        return res.json()["token"]
     else:
         print (res.json()["error"])
 
-def auth():
-    global token
-    global idMod
+def auth(username, password):
     data = { "username" : username, "password" : password }
     url = server + "/auth?token=" + token
     res = requests.post(url, json = data)
     if (res.status_code == 200):
-        idMod = res.json()["idModerator"]
-        token = res.json()["token"]
+        return { "idMod" : res.json()["idModerator"], "token" : res.json()["token"] }
     else:
         print("auth rip")
 
-def createPoll(title):
+def createPoll(token, idMod, title):
     data = { "title" : title }
     url = server + "/mod/"+ str(idMod) + "/poll?token=" + token
     res = requests.post(url, json = data)
     if (res.status_code == 200):
-        idPoll = res.json()["idPoll"]
-        return idPoll
+        return res.json()["idPoll"]
     else:
         print(res.json())
 
-def addQuestion(idPoll, title):
+def addQuestion(token, idPoll, title):
     data = { "title" : title, "details" : "This is a comment", "visibility" : "visible",
             "answersMin" : 1, "answersMax" : 10 }
     url = server + "/mod/"+ str(idMod) + "/poll/" + str(idPoll) + "/question?token=" + token
@@ -48,7 +41,7 @@ def addQuestion(idPoll, title):
     else:
         print(res.json())
 
-def sessionStatus(idPoll, status):
+def sessionStatus(token, idPoll, status):
     data = { "status" : status }
     url = server + "/mod/"+ str(idMod) + "/poll/" + str(idPoll) + "/session?token=" + token
     res = requests.put(url, json = data)
@@ -63,7 +56,6 @@ def connectUser(code):
     url = server + "/connect"
     res = requests.post(url, json = data)
     if (res.status_code == 200):
-        print(res.json())
         return res.json()["token"]
     else:
         print(res.json())
@@ -76,35 +68,4 @@ def getSession(token):
         return res.json()
     else:
         print(res.json())
-
-
-username="guy-laurent"
-password="1234"
-
-register()
-auth()
-
-print("Registered and authenticated")
-print("Token: " + token)
-print("idMod: " + str(idMod))
-print()
-print()
-
-idPoll = createPoll("My poll")
-print("Created poll with id " + str(idPoll))
-
-print()
-print()
-# addQuestion(idPoll, "This is my first question")
-# addQuestion(idPoll, "This is my second question")
-# addQuestion(idPoll, "This is my third question")
-
-code = sessionStatus(idPoll, "open")
-print("Here is the code " + code)
-
-print()
-print()
-
-#userToken = connectUser(code)
-#session = getSession(userToken)
 
