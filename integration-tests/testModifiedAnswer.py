0cdff59fcc711616affb53d05c623a66915592e0
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-12 18:39:46 +0200

    Finish test

--- integration-tests/testModifiedAnswer.py
+++ integration-tests/testModifiedAnswer.py
@@ -1,4 +1,5 @@
 import api
+import sys
 
 reg = api.register("testModifiedAnswer", "1234")
 
@@ -12,23 +13,23 @@ idPoll = poll["idPoll"]
 q1 = api.postQuestion(tokenMod, idMod, idPoll, "Question 1", "visible", 1, 0, 0)
 idQ1 = q1["idQuestion"]
 
-ans11_Title = "haha"
-ans12_Title = "Réponse 2"
-api.postAnswer(tokenMod, idMod, idPoll, idQ1, ans11_Title, "")
-api.postAnswer(tokenMod, idMod, idPoll, idQ1, ans12_Title, "")
+ans1 = [ "haha"
+       , "Réponse 2" ]
+api.postAnswer(tokenMod, idMod, idPoll, idQ1, ans1[0], "")
+api.postAnswer(tokenMod, idMod, idPoll, idQ1, ans1[1], "")
 
 q2 = api.postQuestion(tokenMod, idMod, idPoll, "Question 2", "visible", 1, 0, 0)
 idQ2 = q2["idQuestion"]
-ans21_Title = "Clarisse"
-ans22_Title = "David"
-ans23_Title = "Alexandre"
-ans24_Title = "Matthieu"
-ans25_Title = "Guy-Laurent"
-api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans21_Title, "")
-api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans22_Title, "")
-api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans23_Title, "")
-api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans24_Title, "")
-api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans25_Title, "")
+ans2 = [ "Clarisse"
+       , "David"
+       , "Alexandre"
+       , "Matthieu"
+       , "Guy-Laurent" ]
+api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans2[0], "")
+api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans2[1], "")
+api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans2[2], "")
+api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans2[3], "")
+api.postAnswer(tokenMod, idMod, idPoll, idQ2, ans2[4], "")
 
 code = api.putSessionWithId(tokenMod, idMod, idPoll, "open")["code"]
 
@@ -41,6 +42,19 @@ user_idMod = session["idModerator"]
 user_idPoll = session["idPoll"]
 
 user_questions = api.getQuestion(tokenUser, user_idMod, user_idPoll)
+
+flag = False;
 for question in user_questions:
     if (question["title"] != q1["title"]) and (question["title"] != q2["title"]):
         sys.exit(1)
+
+    answers = api.getAnswer(tokenUser, user_idMod, user_idPoll, question["idQuestion"])
+    for ans in answers:
+        if ans["title"] in (ans1 + ans2):
+            flag = True
+
+if not flag:
+    print("[FAILED] Title from answers should be found in answer list")
+    sys.exit(1)
+else:
+    sys.exit(0)
