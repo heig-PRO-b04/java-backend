0a06e9511bd5e25dd325436adc6d6d46352c0697
David <duprazdavid@gmail.com>
2020-05-20 18:51:28 +0200

    Add answers and question tests

--- src/test/java/ch/heigvd/pro/b04/answers/ServerAnswerTest.java
+++ src/test/java/ch/heigvd/pro/b04/answers/ServerAnswerTest.java
@@ -11,6 +11,7 @@ import ch.heigvd.pro.b04.error.exceptions.ResourceNotFoundException;
 import ch.heigvd.pro.b04.moderators.Moderator;
 import ch.heigvd.pro.b04.moderators.ModeratorRepository;
 import ch.heigvd.pro.b04.participants.Participant;
+import ch.heigvd.pro.b04.participants.ParticipantIdentifier;
 import ch.heigvd.pro.b04.participants.ParticipantRepository;
 import ch.heigvd.pro.b04.polls.ServerPoll;
 import ch.heigvd.pro.b04.polls.ServerPollIdentifier;
@@ -18,6 +19,10 @@ import ch.heigvd.pro.b04.polls.ServerPollRepository;
 import ch.heigvd.pro.b04.questions.QuestionRepository;
 import ch.heigvd.pro.b04.questions.ServerQuestion;
 import ch.heigvd.pro.b04.questions.ServerQuestionIdentifier;
+import ch.heigvd.pro.b04.sessions.ServerSession;
+import ch.heigvd.pro.b04.sessions.SessionIdentifier;
+import ch.heigvd.pro.b04.sessions.SessionState;
+import ch.heigvd.pro.b04.votes.ServerVoteRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -47,6 +52,8 @@ public class ServerAnswerTest {
   QuestionRepository questionRepository;
   @Mock
   AnswerRepository answerRepository;
+  @Mock
+  ServerVoteRepository voteRepository;
 
   @Test
   public void testModeratorCannotAccessAnswersOfOtherModerators() {
@@ -83,15 +90,24 @@ public class ServerAnswerTest {
         .build();
 
     when(participantRepository.findByToken("banuk")).thenReturn(Optional.empty());
+    when(participantRepository.findByToken("chieftain")).thenReturn(Optional.empty());
     when(modoRepo.findByToken("banuk")).thenReturn(Optional.of(ikrie));
+    when(modoRepo.findByToken("chieftain")).thenReturn(Optional.of(aloy));
+    when(modoRepo.findById(aloy.getIdModerator())).thenReturn(Optional.of(aloy));
     lenient().when(pollRepo.findById(identifier)).thenReturn(Optional.of(pollTemp));
     lenient().when(questionRepository.findById(qi1)).thenReturn(Optional.of(q1));
 
+    //right token
+    assertDoesNotThrow(() -> ac.all(1, 123, 1, "chieftain"));
+    //wrong token
     assertThrows(WrongCredentialsException.class, () -> ac.all(1, 123, 1, "banuk"));
+    //wrong parameters
+    assertThrows(ResourceNotFoundException.class, () -> ac.all(1, 123, 3, "chieftain"));
   }
 
   @Test
-  public void testAllEndpointsReturnAllAnswers() {
+  public void testAllAndByIdEndpointsReturnRequiredAnswers()
+      throws ResourceNotFoundException, WrongCredentialsException {
 
     Moderator aloy = Moderator.builder()
         .idModerator(1)
@@ -99,13 +115,22 @@ public class ServerAnswerTest {
         .token("chieftain")
         .build();
 
+    Moderator ikrie = Moderator.builder()
+        .idModerator(2)
+        .username("ikrie")
+        .token("banuk")
+        .build();
+
     ServerPollIdentifier identifier = ServerPollIdentifier.builder()
         .idxModerator(aloy)
         .idPoll(123)
         .build();
 
+    ServerSession session=ServerSession.builder().build();
+
     ServerPoll poll = ServerPoll.builder()
         .idPoll(identifier)
+        .serverSessionSet(Set.of(session))
         .build();
 
     ServerQuestionIdentifier qi1 = ServerQuestionIdentifier.builder()
@@ -138,18 +163,51 @@ public class ServerAnswerTest {
         .title("Just when I destroyed one")
         .build();
 
+    session= ServerSession.builder()
+        .idSession(SessionIdentifier.builder()
+        .idSession(1)
+        .idxPoll(poll).build())
+        .state(SessionState.OPEN)
+        .build();
+
+    Participant varga= Participant.builder()
+        .idParticipant(ParticipantIdentifier.builder()
+            .idxServerSession(session)
+            .idParticipant(1).build())
+        .token("icerail").build();
+
     // Data access.
     when(modoRepo.findById(1)).thenReturn(Optional.of(aloy));
+    when(modoRepo.findById(2)).thenReturn(Optional.of(ikrie));
     when(pollRepo.findById(identifier)).thenReturn(Optional.of(poll));
     when(questionRepository.findById(qi1)).thenReturn(Optional.of(q1));
     lenient().when(answerRepository.findAll())
         .thenReturn(List.of(a1, a2));
+    when(answerRepository.findById(identifier1)).thenReturn(Optional.of(a1));
+    when(answerRepository.findById(identifier2)).thenReturn(Optional.of(a2));
 
     // Token management.
     lenient().when(modoRepo.findByToken("chieftain")).thenReturn(Optional.of(aloy));
+    lenient().when(modoRepo.findByToken("banuk")).thenReturn(Optional.of(ikrie));
     lenient().when(participantRepository.findByToken("chieftain")).thenReturn(Optional.empty());
+    when(participantRepository.findByToken(varga.getToken())).thenReturn(Optional.of(varga));
+    lenient().when(participantRepository.findById(varga.getIdParticipant())).thenReturn(Optional.of(varga));
+    when(voteRepository.findAll()).thenReturn(List.of());
 
+    //moderator token
     assertDoesNotThrow(() -> assertEquals(List.of(a1, a2), ac.all(1, 123, 1, "chieftain")));
+    assertEquals(a1.getTitle(), ac.byId(1,123,1,1,aloy.getToken()).getTitle());
+    assertEquals(a2.getTitle(), ac.byId(1,123,1,2,aloy.getToken()).getTitle());
+
+    //participant token
+    assertDoesNotThrow(() -> assertEquals(List.of(a1, a2), ac.all(1, 123, 1, varga.getToken())));
+    assertEquals(a1.getTitle(), ac.byId(1,123,1,1,varga.getToken()).getTitle());
+
+    //wrong tokens and params
+    assertThrows(WrongCredentialsException.class, () -> assertEquals(List.of(a1, a2),
+        ac.all(1, 123, 1, ikrie.getToken())));
+    assertThrows(ResourceNotFoundException.class, () -> assertEquals(List.of(a1, a2),
+        ac.all(2, 123, 1, ikrie.getToken())));
   }
 
   @Test
@@ -228,8 +286,12 @@ public class ServerAnswerTest {
 
     //Moderator who owns the poll
     assertDoesNotThrow(() -> ac.insertAnswer(1, 123, 1, "t1", a1));
+    //wrong params
+    assertThrows(ResourceNotFoundException.class, () -> ac.insertAnswer(1, 124, 1, "t1", a1));
     //Moderator who does not own the poll
     assertThrows(ResourceNotFoundException.class, () -> ac.insertAnswer(2, 123, 1, "t2", a1));
+    //wrong token
+    assertThrows(WrongCredentialsException.class, () -> ac.insertAnswer(1, 123, 1, "t2", a1));
   }
 
   @Test
@@ -241,6 +303,10 @@ public class ServerAnswerTest {
 
     ServerQuestionIdentifier iq = ServerQuestionIdentifier.builder()
         .idServerQuestion(1)
+        .idxPoll(pollTemp).build();
+
+    ServerQuestionIdentifier iq2 = ServerQuestionIdentifier.builder()
+        .idServerQuestion(2)
         .idxPoll(pollTemp).build();
 
     ServerAnswerIdentifier ia = ServerAnswerIdentifier.builder()
@@ -282,6 +348,8 @@ public class ServerAnswerTest {
     when(modoRepo.findByToken("t1")).thenReturn(Optional.of(aloy));
     when(modoRepo.findByToken("t2")).thenReturn(Optional.of(ikrie));
     when(questionRepository.findById(Mockito.any())).thenReturn(Optional.of(c1));
+    when(questionRepository.findById(ServerQuestionIdentifier.builder()
+        .idServerQuestion(2).idxPoll(pollTemp).build())).thenReturn(Optional.empty());
     when(pollRepo.findById(Mockito.any())).thenReturn(Optional.empty());
     when(pollRepo.findById(
         ServerPollIdentifier.builder()
@@ -291,9 +359,14 @@ public class ServerAnswerTest {
     when(answerRepository.save(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
 
     //Moderator who owns the poll
-    assertEquals("Just when I destroyed one",
+    assertEquals(a2.getTitle(),
         ac.updateAnswer(1, 123, 1, 1, "t1", a2).getTitle());
+    //wrong params
+    assertThrows(ResourceNotFoundException.class, ()-> ac.updateAnswer(1, 123, 2, 1, "t1", a2));
     //Moderator who does not own the poll
+    assertThrows(ResourceNotFoundException.class,
+        () -> ac.updateAnswer(2, 123, 1, 1, "t2", a2));
+    //wrong token
     assertThrows(WrongCredentialsException.class,
         () -> ac.updateAnswer(1, 123, 1, 1, "t2", a2));
   }
