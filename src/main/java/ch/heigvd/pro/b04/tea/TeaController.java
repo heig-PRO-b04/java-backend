40d4769452106efbe23360ffb2a045443f1caf32
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-25 15:39:17 +0200

    Add documentation

--- src/main/java/ch/heigvd/pro/b04/tea/TeaController.java
+++ src/main/java/ch/heigvd/pro/b04/tea/TeaController.java
@@ -8,12 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TeaController {
 
+  /**
+   * If coffee is asked, answer that we cannot !
+   * https://tools.ietf.org/html/rfc2324#section-2.3.2
+   * @throws TeapotException Always
+   */
   @GetMapping("/coffee")
   public void makeCoffee()
       throws TeapotException {
     throw new TeapotException();
   }
 
+  /**
+   * If we cannot brew coffee, we can make Tea.
+   * @return A T
+   */
   @GetMapping("/tea")
   public ServerMessage makeTea() {
     return ServerMessage.builder()
